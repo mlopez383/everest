a2e4822923181e309f9fb0e0d174d1902a45936c
Miguel Lopez <tramitador20@gmail.com>
2019-08-06 16:29:04 -0400

    backend: initial project using Spring Boot

---
+++ backend/src/main/java/com/rental/advertisements/AdvertisementsApplication.java
@@ -0,0 +1,13 @@
+package com.rental.advertisements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertisementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertisementsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rental/advertisements/AdvertisementsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rental.advertisements;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdvertisementsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
