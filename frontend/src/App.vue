c8d35f9f0166e0f4de6848694b05902d807e9383
Miguel Lopez <tramitador20@gmail.com>
2019-08-06 23:10:07 -0400

    Implement "Sign Up" UI

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,20 +4,23 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <a href="#" v-b-modal.modal-login>Log In</a> |
-      <a href="#">Sign Up</a>
+      <a href="#" v-b-modal.modal-signup>Sign Up</a>
     </div>
     <router-view />
     <LogIn />
+    <SignUp />
   </div>
 </template>
 
 <script>
 import LogIn from "@/components/LogIn.vue";
+import SignUp from "@/components/SignUp.vue";
 
 export default {
   name: "app",
   components: {
-    LogIn
+    LogIn,
+    SignUp
   }
 };
 </script>
--- frontend/src/components/SignUp.vue
+++ frontend/src/components/SignUp.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
     <b-modal 
-      id="modal-login"
-      ref="loginModal"
+      id="modal-signup"
+      ref="signupModal"
       title="Log In"
       @show="resetModal"
       @hidden="resetModal"
     >
       <form @submit.stop.prevent="handleSubmit">
+        <b-form-group
+          :state="nameState"
+          label="Name"
+          label-for="name-input"
+          invalid-feedback="Name is required"
+        >
+          <b-form-input
+            id="name-input"
+            ref="nameinput"
+            v-model="name"
+            :state="nameState"
+            required
+          >
+          </b-form-input>
+        </b-form-group>
         <b-form-group
           :state="emailState"
           label="E-mail"
@@ -38,10 +53,25 @@
           >
           </b-form-input>
         </b-form-group>
+        <b-form-group
+          :state="password2State"
+          label="Password Again"
+          label-for="password2-input"
+          invalid-feedback="Password is required Again"
+        >
+          <b-form-input
+            id="password2-input"
+            ref="password2input"
+            v-model="passw2"
+            :state="password2State"
+            required
+          >
+          </b-form-input>
+        </b-form-group>
       </form>
       <div slot="modal-footer" class="w-100">
         <b-button variant="primary" @click="handleOk">
-          LOGIN
+          SIGN UP
         </b-button>
       </div>
     </b-modal>
@@ -50,30 +80,44 @@
 
 <script>
 export default {
-  name: "LogIn",
+  name: "SignUp",
   data() {
     return {
+       name: '',
+       nameState: null,
        email: '',
        emailState: null,
        passw: '',
        passwordState: null,
+       passw2: '',
+       password2State: null,
     };
   },
   methods: {
     checkFormValidity() {
+        const validName = this.$refs.nameinput.checkValidity();
+        this.nameState = validName ? 'valid' : 'invalid';
+        
         const validEmail = this.$refs.emailinput.checkValidity();
         this.emailState = validEmail ? 'valid' : 'invalid';
 
         const validPassword = this.$refs.passwordinput.checkValidity();
         this.passwordState = validPassword ? 'valid' : 'invalid';
 
-        return validEmail & validPassword;
+        const validPassword2 = this.$refs.password2input.checkValidity();
+        this.password2State = validPassword2 ? 'valid' : 'invalid';
+        
+        return validName & validEmail & validPassword & validPassword2;
     },
     resetModal() {
+      this.name = '';
+      this.nameState = null;
       this.email = '';
       this.emailState = null;
       this.passw = '';
       this.passwordState = null;
+      this.passw2 = '';
+      this.password2State = null;
     },
     handleOk(bvModalEvt) {
       bvModalEvt.preventDefault();
@@ -85,7 +129,7 @@ export default {
       }
 
       this.$nextTick(() => {
-        this.$refs.loginModal.hide();
+        this.$refs.signupModal.hide();
       });
     }
   }
