d6bb282aeaffb8db6a9156691762c55167b7777c
Miguel Lopez <tramitador20@gmail.com>
2019-08-09 19:16:01 -0400

    Implement basic REST API on the backend side

---
+++ backend/src/main/java/com/rental/advertisements/controllers/UserController.java
@@ -0,0 +1,44 @@
+package com.rental.advertisements;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+    @RequestMapping (
+            value = "login/{username}/{password}",
+            method = RequestMethod.GET,
+            produces = {"application/JSON"}
+    )
+    @ResponseBody
+    String login(@PathVariable String username, @PathVariable String password) {
+        /*TODO: Implement this using the database*/
+        if (username.equals("example@gmail.com") && password.equals("123456")) {
+            return "{\"response\" : \"success\"}";
+        } else {
+            return "{\"response\" : \"failed\"}";
+        }
+    }
+
+    @RequestMapping (value = "signup", method = RequestMethod.POST,
+            consumes = {"application/JSON"},
+            produces = {"application/JSON"}
+    )
+    @ResponseBody
+    String signup(@RequestBody String body){
+        /*TODO: Implement this using the database*/
+        System.out.println(body);
+        return "{\"response\" : \"created\"}";
+    }
+
+    @RequestMapping (value = "", method = RequestMethod.GET)
+    public String user() {
+        /*TODO: Implement this*/
+        return "Error 404 : User";
+    }
+}
